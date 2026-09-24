1c47ac8deba4692938347fa89e85b6ea9ee1ea69
Horacio Sutara <rhsuty@gmail.com>
2026-09-24 01:16:44 -0300

    migracion de capacidad de bloque_agenda

---
+++ prisma/migrations/20260924041617_eliminar_capacidad_bloque_agenda/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `capacidad` on the `bloque_agenda` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "bloque_agenda" DROP COLUMN "capacidad";
